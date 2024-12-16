9da5031c150d8de7b76e41d74cb1f006b38973ce
Lucas Paulino <lucaspaulinoh@hotmail.com>
2024-12-16 14:38:05 -0300

    gg wp 2

--- src/components/technology/index.tsx
+++ src/components/technology/index.tsx
@@ -15,7 +15,7 @@ interface TechnologyProps {
 
 const Technology = (props: TechnologyProps) => {
   const { tecInfo } = props;
-  const ICON_SIZE = 60;
+  const ICON_SIZE = 50;
 
   const [iconFill, setIconFill] = useState(WHITE);
 
